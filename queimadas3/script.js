90f33aab571aa064241072fd0cec21f42a242063
Linda Christi <lindafreitasbastos@gmail.com>
2024-09-23 21:20:19 -0300

    Add files via upload

---
+++ queimadas3/script.js
@@ -0,0 +1,33 @@
+// Scroll suave ao clicar nos links do menu :)
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop - 50,
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Animações ao rolar a página :)
+const sections = document.querySelectorAll('section');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+sections.forEach(section => {
+    observer.observe(section);
+});
